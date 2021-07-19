e67397425f92e252a721c9c6f8d5d58651da899d
Kiran Rai <travestroy007@gmail.com>
2021-07-19 21:19:59 +0545

    adding suspense

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,72 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
-import LoginScreen from './screens/LoginScreen'
-import RegisterScreen from './screens/RegisterScreen'
-import ProfileScreen from './screens/ProfileScreen'
-import ShippingScreen from './screens/ShippingScreen'
-import PaymentScreen from './screens/PaymentScreen'
-import PlaceOrderScreen from './screens/PlaceOrderScreen'
-import OrderScreen from './screens/OrderScreen'
-import UserListScreen from './screens/UserListScreen'
-import UserEditScreen from './screens/UserEditScreen'
-import ProductListScreen from './screens/ProductListScreen'
-import ProductEditScreen from './screens/ProductEditScreen'
-import OrderListScreen from './screens/OrderListScreen'
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const HomeScreen = lazy(() => import("./screens/HomeScreen"));
+const ProductScreen = lazy(() => import("./screens/ProductScreen"));
+const UserListScreen = lazy(() => import("./screens/UserListScreen"));
+const CartScreen = lazy(() => import("./screens/CartScreen"));
+const LoginScreen = lazy(() => import("./screens/LoginScreen"));
+const RegisterScreen = lazy(() => import("./screens/RegisterScreen"));
+const ProfileScreen = lazy(() => import("./screens/ProfileScreen"));
+const ShippingScreen = lazy(() => import("./screens/ShippingScreen"));
+const PaymentScreen = lazy(() => import("./screens/PaymentScreen"));
+const PlaceOrderScreen = lazy(() => import("./screens/PlaceOrderScreen"));
+const OrderScreen = lazy(() => import("./screens/OrderScreen"));
+const UserEditScreen = lazy(() => import("./screens/UserEditScreen"));
+const ProductListScreen = lazy(() => import("./screens/ProductListScreen"));
+const ProductEditScreen = lazy(() => import("./screens/ProductEditScreen"));
+const OrderListScreen = lazy(() => import("./screens/OrderListScreen"));
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <Route path='/order/:id' component={OrderScreen} />
-          <Route path='/shipping' component={ShippingScreen} />
-          <Route path='/payment' component={PaymentScreen} />
-          <Route path='/placeorder' component={PlaceOrderScreen} />
-          <Route path='/login' component={LoginScreen} />
-          <Route path='/register' component={RegisterScreen} />
-          <Route path='/profile' component={ProfileScreen} />
-          <Route path='/product/:id' component={ProductScreen} />
-          <Route path='/cart/:id?' component={CartScreen} />
-          <Route path='/admin/userlist' component={UserListScreen} />
-          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
-          <Route
-            path='/admin/productlist'
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path='/admin/productlist/:pageNumber'
-            component={ProductListScreen}
-            exact
-          />
-          <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
-          <Route path='/admin/orderlist' component={OrderListScreen} />
-          <Route path='/search/:keyword' component={HomeScreen} exact />
-          <Route path='/page/:pageNumber' component={HomeScreen} exact />
-          <Route
-            path='/search/:keyword/page/:pageNumber'
-            component={HomeScreen}
-            exact
-          />
-          <Route path='/' component={HomeScreen} exact />
+          <Suspense fallback={"<div></div>"}>
+            <Route path="/order/:id" component={OrderScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/login" component={LoginScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/admin/userlist" component={UserListScreen} />
+            <Route path="/admin/user/:id/edit" component={UserEditScreen} />
+            <Route
+              path="/admin/productlist"
+              component={ProductListScreen}
+              exact
+            />
+            <Route
+              path="/admin/productlist/:pageNumber"
+              component={ProductListScreen}
+              exact
+            />
+            <Route
+              path="/admin/product/:id/edit"
+              component={ProductEditScreen}
+            />
+            <Route path="/admin/orderlist" component={OrderListScreen} />
+            <Route path="/search/:keyword" component={HomeScreen} exact />
+            <Route path="/page/:pageNumber" component={HomeScreen} exact />
+            <Route
+              path="/search/:keyword/page/:pageNumber"
+              component={HomeScreen}
+              exact
+            />
+            <Route path="/" component={HomeScreen} exact />
+          </Suspense>
         </Container>
       </main>
       <Footer />
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
